d831bf42930918f42f171a7af6e2e885227ab4c6
bunsasspr <buiminhtriet57@gmail.com>
2026-08-08 09:09:19 +0700

    Update debug.lua

--- debug.lua
+++ debug.lua
@@ -1,49 +1,34 @@
-local TARGET = "=QiUmItaUgqarWtuEiLaRxBKPQ"
-
-print("========== LUAST TARGET ==========")
+print("========== NEW FUNCTIONS ==========")
 
 local count = 0
-local seen = {}
 
 for _, fn in ipairs(getgc(true)) do
-    if type(fn) == "function" and not seen[fn] then
-        seen[fn] = true
+    if type(fn) == "function" and not _G.__before[fn] then
+        count += 1
 
         local ok, info = pcall(debug.getinfo, fn)
 
-        if ok and info and info.source == TARGET then
-            count += 1
-
+        if ok and info then
             print("")
-            print("===== FUNCTION #" .. count .. " =====")
+            print("----- NEW #" .. count .. " -----")
             print("Function:", tostring(fn))
             print("Name:", tostring(info.name))
+            print("Source:", tostring(info.source))
             print("Line:", tostring(info.currentline))
 
-            local okc, constants = pcall(debug.getconstants, fn)
+            if debug.getconstants then
+                local okc, constants = pcall(debug.getconstants, fn)
 
-            if okc and type(constants) == "table" then
-                for i, value in pairs(constants) do
-                    print(
-                        "CONST[" .. tostring(i) .. "]:",
-                        typeof(value),
-                        tostring(value)
-                    )
-                end
-            end
-
-            if debug.getupvalues then
-                local oku, upvalues = pcall(debug.getupvalues, fn)
-
-                if oku and type(upvalues) == "table" then
-                    print("-- UPVALUES --")
-
-                    for i, value in pairs(upvalues) do
-                        print(
-                            "UPVALUE[" .. tostring(i) .. "]:",
-                            typeof(value),
-                            tostring(value)
-                        )
+                if okc and type(constants) == "table" then
+                    for i, v in pairs(constants) do
+                        if type(v) == "string"
+                            or type(v) == "number"
+                            or type(v) == "boolean" then
+                            print(
+                                "CONST[" .. tostring(i) .. "]:",
+                                tostring(v)
+                            )
+                        end
                     end
                 end
             end
@@ -52,5 +37,5 @@ for _, fn in ipairs(getgc(true)) do
 end
 
 print("")
-print("TOTAL:", count)
-print("==================================")
+print("NEW FUNCTIONS:", count)
+print("===================================")
